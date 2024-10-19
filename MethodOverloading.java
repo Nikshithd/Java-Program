d29d1572289eda9b2de5de81977d74ab0450eec3
Nikshithd <nikshith.shetty.d.m@gmail.com>
2024-10-19 19:37:11 +0530

    Method Overloading Example

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -1,7 +1,18 @@
 public class MethodOverloading {
     public static void main(String[] args) {
-        System.err.println("Nikshith");
+        sumOfTwoNumber();
+        sumOfTwoNumber(10,20);
     }
 
-    
+    public static void sumOfTwoNumber(int a, int b) {
+        int sum = a + b;
+        System.err.println("Sum of two Number " +sum);
+    }
+
+    public static void sumOfTwoNumber() {
+        int a = 30;
+        int b = 40;
+        int sum = a + b;
+        System.err.println("Sum of two Number " +sum);
+    }
 }
